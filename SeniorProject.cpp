f42de28c06d66fcb51fbf1c3ca88f25d3acdd3ef
Garrett <nadauldgarrett@gmail.com>
2025-03-10 19:08:26 -0600

    removed warning for motors, declared fuctions ahead of main

--- SeniorProject.cpp
+++ SeniorProject.cpp
@@ -42,10 +42,8 @@ int16_t leveling; // State of input switch
 
 
 // Declares user-defined helper functions.
-// The definition/implementations of these functions are at the bottom of the sketch.
-bool SynchronizedMove(int32_t distance);
-void PrintAlerts();
-void HandleAlerts();
+void MoveDistanceX(int32_t distance);
+void MoveDistanceY(int32_t distance);
 
 
 /*------------------------------------------------------------------------------
@@ -72,11 +70,26 @@ int main() {
     bool LevelFlag = false;	//Used to set level position of first iteration of loop
     bool Xflag = true;	//Used to determine which axis to move first
     double LevelX, LevelY, Xpos, Ypos = 0.0;
+    int16_t adcSUM, adcY, adcX = 0;
  
     while (true) {
 
-        double inputVoltageSUM, inputVoltageY, inputVoltageX = 0.0;
-        inputVoltageSUM, inputVoltageY, inputVoltageX = readAnalogInputs();	//Read analog input from sensor
+        inputVoltageSUM = 0.0;
+        inputVoltageY = 0.0;
+        inputVoltageX = 0.0;
+        // Read the analog input (A-9 through A-12 may be configured as analog
+        // inputs).
+        adcSUM = ConnectorA12.State();
+        // Convert the reading to a voltage.
+        inputVoltageSUM = 10.0 * adcSUM / ((1 << adcResolution) - 1);
+        
+        adcY = ConnectorA11.State();
+        // Convert the reading to a voltage.
+        inputVoltageY = 10.0 * adcY / ((1 << adcResolution) - 1);
+        
+        adcX = ConnectorA10.State();
+        // Convert the reading to a voltage.
+        inputVoltageX = 10.0 * adcX / ((1 << adcResolution) - 1);
 
         if (leveling) {	//Once switch has been set to the on position the bed is level, enter automated leveling state
             if (LevelFlag==false) {	//If either level flag is false calibrate level position to be compared to new level data and set flag to true
@@ -120,38 +133,10 @@ int main() {
         Delay_ms(1000);		// Wait a 1 second before the next reading.
     }
 }
+ 
 
-/*------------------------------------------------------------------------------
- * ReadAnalogInputs
- *
- *    Used to get voltages from A9-A12
- *   
- *
- * Parameters:
- *    None
- *
- * Returns:
- *    SUM voltage
- *    X voltage
- *    Y voltage
- */
-double readAnalogInputs() {
-    // Read the analog input (A-9 through A-12 may be configured as analog
-        // inputs).
-        int16_t adcSUM = ConnectorA12.State();
-        // Convert the reading to a voltage.
-        double inputVoltageSUM = 10.0 * adcSUM / ((1 << adcResolution) - 1);
-        
-        int16_t adcY = ConnectorA11.State();
-        // Convert the reading to a voltage.
-        double inputVoltageY = 10.0 * adcY / ((1 << adcResolution) - 1);
-        
-        int16_t adcX = ConnectorA10.State();
-        // Convert the reading to a voltage.
-        double inputVoltageX = 10.0 * adcX / ((1 << adcResolution) - 1);
 
-    return inputVoltageSUM, inputVoltageY, inputVoltageX;
-}
+
  
 /*------------------------------------------------------------------------------
  * MoveDistanceX
@@ -166,81 +151,82 @@ double readAnalogInputs() {
  *
  * Returns: True/False depending on whether the move was successfully triggered.
  */
-bool MoveDistanceX(int32_t distance) {
+void MoveDistanceX(int32_t distance) {
     // Check if a motorX alert is currently preventing motion
     // Clear alert if configured to do so 
-    if (motorX.StatusReg().bit.AlertsPresent) {
-        SerialPort.SendLine("MotorX alert detected.");       
-        PrintAlerts();
-        if(HANDLE_ALERTS){
-            HandleAlerts();
-        } else {
-            SerialPort.SendLine("Enable automatic alert handling by setting HANDLE_ALERTS to 1.");
-        }
-        SerialPort.SendLine("Move canceled.");      
-        SerialPort.SendLine();
-        return false;
-    }
+    // if (motorX.StatusReg().bit.AlertsPresent) {
+    //     SerialPort.SendLine("MotorX alert detected.");       
+    //     PrintAlerts();
+    //     if(HANDLE_ALERTS){
+    //         HandleAlerts();
+    //     } else {
+    //         SerialPort.SendLine("Enable automatic alert handling by setting HANDLE_ALERTS to 1.");
+    //     }
+    //     SerialPort.SendLine("Move canceled.");      
+    //     SerialPort.SendLine();
+    //     return false;
+    // }
  
-    SerialPort.Send("Moving distance: ");
-    SerialPort.SendLine(distance);
+    // SerialPort.Send("Moving distance: ");
+    // SerialPort.SendLine(distance);
  
     // Command the move of incremental distance
     motorX.Move(distance);
  
     // Waits for HLFB to assert (signaling the move has successfully completed)
-    SerialPort.SendLine("Moving.. Waiting for HLFB");
+    // SerialPort.SendLine("Moving.. Waiting for HLFB");
     while ( (!motorX.StepsComplete() || motorX.HlfbState() != MotorDriver::HLFB_ASSERTED) &&
             !motorX.StatusReg().bit.AlertsPresent) {
         continue;
     }
-    // Check if motorX alert occurred during move
-    // Clear alert if configured to do so 
-    if (motorX.StatusReg().bit.AlertsPresent) {
-        SerialPort.SendLine("MotorX alert detected.");       
-        PrintAlerts();
-        if(HANDLE_ALERTS){
-            HandleAlerts();
-        } else {
-            SerialPort.SendLine("Enable automatic fault handling by setting HANDLE_ALERTS to 1.");
-        }
-        SerialPort.SendLine("Motion may not have completed as expected. Proceed with caution.");
-        SerialPort.SendLine();
-        return false;
-    } else {
-        SerialPort.SendLine("Move Done");
-        return true;
-    }
+    // // Check if motorX alert occurred during move
+    // // Clear alert if configured to do so 
+    // if (motorX.StatusReg().bit.AlertsPresent) {
+    //     SerialPort.SendLine("MotorX alert detected.");       
+    //     PrintAlerts();
+    //     if(HANDLE_ALERTS){
+    //         HandleAlerts();
+    //     } else {
+    //         SerialPort.SendLine("Enable automatic fault handling by setting HANDLE_ALERTS to 1.");
+    //     }
+    //     SerialPort.SendLine("Motion may not have completed as expected. Proceed with caution.");
+    //     SerialPort.SendLine();
+    //     return false;
+    // } else {
+    //     SerialPort.SendLine("Move Done");
+    //     return true;
+    // }
 }
 
-bool MoveDistanceY(int32_t distance) {
+void MoveDistanceY(int32_t distance) {
     // Check if a motorY alert is currently preventing motion
     // Clear alert if configured to do so 
-    if (motorY.StatusReg().bit.AlertsPresent) {
-        SerialPort.SendLine("MotorY alert detected.");       
-        PrintAlerts();
-        if(HANDLE_ALERTS){
-            HandleAlerts();
-        } else {
-            SerialPort.SendLine("Enable automatic alert handling by setting HANDLE_ALERTS to 1.");
-        }
-        SerialPort.SendLine("Move canceled.");      
-        SerialPort.SendLine();
-        return false;
-    }
+    // if (motorY.StatusReg().bit.AlertsPresent) {
+    //     SerialPort.SendLine("MotorY alert detected.");       
+    //     PrintAlerts();
+    //     if(HANDLE_ALERTS){
+    //         HandleAlerts();
+    //     } else {
+    //         SerialPort.SendLine("Enable automatic alert handling by setting HANDLE_ALERTS to 1.");
+    //     }
+    //     SerialPort.SendLine("Move canceled.");      
+    //     SerialPort.SendLine();
+    //     return false;
+    // }
  
-    SerialPort.Send("Moving distance: ");
-    SerialPort.SendLine(distance);
+    // SerialPort.Send("Moving distance: ");
+    // SerialPort.SendLine(distance);
  
     // Command the move of incremental distance
     motorY.Move(distance);
  
     // Waits for HLFB to assert (signaling the move has successfully completed)
-    SerialPort.SendLine("Moving.. Waiting for HLFB");
+    // SerialPort.SendLine("Moving.. Waiting for HLFB");
     while ( (!motorY.StepsComplete() || motorY.HlfbState() != MotorDriver::HLFB_ASSERTED) &&
             !motorY.StatusReg().bit.AlertsPresent) {
         continue;
     }
+    /*
     // Check if motorY alert occurred during move
     // Clear alert if configured to do so 
     if (motorY.StatusReg().bit.AlertsPresent) {
@@ -257,65 +243,5 @@ bool MoveDistanceY(int32_t distance) {
     } else {
         SerialPort.SendLine("Move Done");
         return true;
-    }
+    }*/
 }
-//------------------------------------------------------------------------------
- 
- 
-/*------------------------------------------------------------------------------
- * PrintAlerts
- *
- *    Prints active alerts.
- *
- * Parameters:
- *    requires "motor" to be defined as a ClearCore motor connector
- *
- * Returns: 
- *    none
- */
- void PrintAlerts(){
-    // report status of alerts
-    SerialPort.SendLine("Alerts present: ");
-    if(motor.AlertReg().bit.MotionCanceledInAlert){
-        SerialPort.SendLine("    MotionCanceledInAlert "); }
-    if(motor.AlertReg().bit.MotionCanceledPositiveLimit){
-        SerialPort.SendLine("    MotionCanceledPositiveLimit "); }
-    if(motor.AlertReg().bit.MotionCanceledNegativeLimit){
-        SerialPort.SendLine("    MotionCanceledNegativeLimit "); }
-    if(motor.AlertReg().bit.MotionCanceledSensorEStop){
-        SerialPort.SendLine("    MotionCanceledSensorEStop "); }
-    if(motor.AlertReg().bit.MotionCanceledMotorDisabled){
-        SerialPort.SendLine("    MotionCanceledMotorDisabled "); }
-    if(motor.AlertReg().bit.MotorFaulted){
-        SerialPort.SendLine("    MotorFaulted ");
-    }
- }
-//------------------------------------------------------------------------------
- 
- 
-/*------------------------------------------------------------------------------
- * HandleAlerts
- *
- *    Clears alerts, including motor faults. 
- *    Faults are cleared by cycling enable to the motor.
- *    Alerts are cleared by clearing the ClearCore alert register directly.
- *
- * Parameters:
- *    requires "motor" to be defined as a ClearCore motor connector
- *
- * Returns: 
- *    none
- */
- void HandleAlerts(){
-    if(motor.AlertReg().bit.MotorFaulted){
-        // if a motor fault is present, clear it by cycling enable
-        SerialPort.SendLine("Faults present. Cycling enable signal to motor to clear faults.");
-        motor.EnableRequest(false);
-        Delay_ms(10);
-        motor.EnableRequest(true);
-    }
-    // clear alerts
-    SerialPort.SendLine("Clearing alerts.");
-    motor.ClearAlerts();
- }
-//------------------------------------------------------------------------------
